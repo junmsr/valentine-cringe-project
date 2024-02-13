f9a9f2922093aedecdd5d8637e5d18c760c98b57
admin <junmsr.github@gmail.com>
2024-02-13 23:12:36 +0800

    valentine commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const go = document.querySelector('.go');
+const no = document.querySelector('.no');
+const question = document.querySelector('.question');
+const body = document.querySelector('.body');
+const chika = document.querySelector('.chika');
+
+go.addEventListener('click', () => {
+    question.innerHTML = 'Yiii >.<';
+    chika.src = 'gifs/chika-yes.gif';
+
+});
+
+no.addEventListener('mouseover', () =>{
+    const i = Math.floor(Math.random() * 500) + 1;
+
+    const j = Math.floor(Math.random() * 500) + 1;
+
+    no.style.left = i + 'px';
+    no.style.top = j + 'px';
+});
